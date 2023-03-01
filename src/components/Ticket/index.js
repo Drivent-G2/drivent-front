8a4ed4a0b3592164262acb8c838fc0038ce2f566
Lucas <lucasreis64@gmail.com>
2023-03-01 04:27:34 -0300

    feat: payment

---
+++ src/components/Ticket/index.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export default function Ticket(params) {
+  return (
+    <TicketContainer>
+      <TicketType>
+        <h2>Primeiro, escolha sua modalidade de ingresso</h2>
+      </TicketType>
+      <HotelChoice>
+
+      </HotelChoice>
+    </TicketContainer>
+  );
+};
+
+const TicketContainer = styled.div`
+  
+`;
+
+const TicketType = styled.div`
+  
+`;
+
+const HotelChoice = styled.div`
+  
+`;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,66 @@
+import { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Ticket from '../../../components/Ticket';
+import UserContext from '../../../contexts/UserContext';
+import { getPersonalInformations } from '../../../services/enrollmentApi';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { userData } = useContext(UserContext);
+  const [enroll, setEnroll] = useState(false);
+
+  useEffect(async() => {
+    try {
+      await getPersonalInformations(userData.token);
+      setEnroll(true);
+    } catch (err) {
+      setEnroll(false);
+    }
+  });
+
+  return (
+    <TicketAndPayment>
+      <Title>Ingresso e Pagamento</Title>
+      <PaymentContainer>{enroll?<Ticket/>:<div className="center"><h1 className='advise'>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</h1></div>}</PaymentContainer>
+    </TicketAndPayment>
+  );
 }
+
+const TicketAndPayment = styled.div`
+  height: 100%;
+  width: 100%;
+  position: relative;
+
+`;
+
+const Title = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 34px;
+`;
+
+const PaymentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  .center{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    .advise{
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 20px;
+      color: #8E8E8E;
+      width: 55%;
+      text-align: center;
+    }
+  }
+`;
+
