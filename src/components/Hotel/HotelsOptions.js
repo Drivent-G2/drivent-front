4370bc054827af179aaf5a9b005969c71a78a050
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2023-03-13 10:55:38 -0300

    feat: hotel option component

--- src/components/Hotel/HotelsOptions.js
+++ src/components/Hotel/HotelsOptions.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import HotelInfo from './HotelInfo';
 
 export default function HotelsOptions(params) {
   const { hotelsOptionsList, hotelSelectedId, setHotelSelectedId } = params;
@@ -9,33 +10,35 @@ export default function HotelsOptions(params) {
         {hotelsOptionsList.map(h => {
           if(h.id === hotelSelectedId) {
             return(
-              <Option key={h.id} select={true} >
-                <img src={h.image} alt=''/>
-                <h1> {h.name} </h1>
-                <div>
-                  <h2>Tipos de acomodação:</h2>
-                  <h3>Single e Double</h3>
-                </div>
-                <div>
-                  <h2>Vagas disponíveis:</h2>
-                  <h3>Single e Double</h3>
-                </div>
-              </Option>
+              <HotelInfo h={h} select={true} setHotelSelectedId={setHotelSelectedId} />
+              // <Option key={h.id} select={true} >
+              //   <img src={h.image} alt=''/>
+              //   <h1> {h.name} </h1>
+              //   <div>
+              //     <h2>Tipos de acomodação:</h2>
+              //     <h3>Single e Double</h3>
+              //   </div>
+              //   <div>
+              //     <h2>Vagas disponíveis:</h2>
+              //     <h3>Single e Double</h3>
+              //   </div>
+              // </Option>
             );
           }
           return(
-            <Option key={h.id} onClick={() => setHotelSelectedId(h.id)}>
-              <img src={h.image} alt=''/>
-              <h1> {h.name} </h1>
-              <div>
-                <h2>Tipos de acomodação:</h2>
-                <h3>Single e Double</h3>
-              </div>
-              <div>
-                <h2>Vagas disponíveis:</h2>
-                <h3>Single e Double</h3>
-              </div>
-            </Option>
+            <HotelInfo h={h} select={false} setHotelSelectedId={setHotelSelectedId} />
+            // <Option key={h.id} onClick={() => setHotelSelectedId(h.id)}>
+            //   <img src={h.image} alt=''/>
+            //   <h1> {h.name} </h1>
+            //   <div>
+            //     <h2>Tipos çslkfdjg de acomodação:</h2>
+            //     <h3>Single e Double</h3>
+            //   </div>
+            //   <div>
+            //     <h2>Vagas disponíveis:</h2>
+            //     <h3>Single e Double</h3>
+            //   </div>
+            // </Option>
           );
         })}
       </div>
@@ -57,34 +60,34 @@ const HotelsChoice = styled.div`
     }
 `;
 
-const Option = styled.div`
-    background-color: ${params => params.select ? '#FFEED2' : '#EBEBEB'};
-    width: 196px;
-    height: 264px;
-    padding: 14px;
-    box-sizing:border-box;
-    margin-right: 19px;
-    border-radius: 10px;
-    img {
-        width: 168px;
-        height: 109px;
-        border-radius: 5px;
-    }
-    h1 {
-        margin: 10px 0;
-        font-size: 20px;
-        font-weight: 400;
-    }
-    div {
-        margin-bottom: 14px;
-    }
-    h2 {
-        font-size: 12px;
-        font-weight: 700;
-        margin-bottom: 4px;
-    }
-    h3 {
-        font-size:12px;
-        font-weight: 400;
-    }
-`;
+// const Option = styled.div`
+//     background-color: ${params => params.select ? '#FFEED2' : '#EBEBEB'};
+//     width: 196px;
+//     height: 264px;
+//     padding: 14px;
+//     box-sizing:border-box;
+//     margin-right: 19px;
+//     border-radius: 10px;
+//     img {
+//         width: 168px;
+//         height: 109px;
+//         border-radius: 5px;
+//     }
+//     h1 {
+//         margin: 10px 0;
+//         font-size: 20px;
+//         font-weight: 400;
+//     }
+//     div {
+//         margin-bottom: 14px;
+//     }
+//     h2 {
+//         font-size: 12px;
+//         font-weight: 700;
+//         margin-bottom: 4px;
+//     }
+//     h3 {
+//         font-size:12px;
+//         font-weight: 400;
+//     }
+// `;
--- src/hooks/api/useHotel.js
+++ src/hooks/api/useHotel.js
@@ -1,11 +1,16 @@
 import { getHotel } from '../../services/hotelApi';
 import useAsync from '../useAsync';
 import useToken from '../useToken';
-
+ 
 export function useHotel() {
   const token = useToken();
 
   const hotels = useAsync(() => getHotel(token));
 
   return hotels.data;
+};
+
+export function getHotelsInfo() {
+  const token = useToken();
+  return('toma o token', token);
 };
