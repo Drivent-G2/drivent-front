f8a7d70c07f0231fb03e160d46c9734333a76bf9
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2023-03-17 10:17:02 -0300

    feat: auditorys static layout without activities box

--- src/components/Activities/Auditory.js
+++ src/components/Activities/Auditory.js
@@ -2,30 +2,58 @@ import styled from 'styled-components';
 
 export default function Auditorys(params) {
   return(
-    <>
-      <AuditoryType>
-        <h1 className='auditoryType'>Auditório Principal</h1>
-        <div className='activitiesBoxList'></div>
-      </AuditoryType>
-    </>
+    <AuditoryList>
+      <div className='auditoryType'>
+        <h1 className='auditoryHeader'>Auditório Principal</h1>
+        <div className='activitiesBoxList'>
+          <Activity/>
+        </div>
+      </div>
+
+      <div className='auditoryType'>
+        <h1 className='auditoryHeader'>Auditório Lateral</h1>
+        <div className='activitiesBoxList'>
+          <Activity/>
+        </div>
+      </div>
+
+      <div className='auditoryType'>
+        <h1 className='auditoryHeader'>Sala de Workshop </h1>
+        <div className='activitiesBoxList'>
+          <Activity/>
+        </div>
+      </div>
+    </AuditoryList>
   );
 };
 
-const AuditoryType = styled.div`
-    background-color: red;
-    width: 288px;
-    height: 390px;
+const AuditoryList = styled.div`
     display: flex;
-    flex-direction: column;
-    align-items: center;
     .auditoryType {
-        font-size: 17px;
-        color: #7b7b7b;
-        margin-bottom: 12px;
+        width: 288px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .auditoryHeader {
+            font-size: 17px;
+            color: #7b7b7b;
+            margin-bottom: 13px;
+        }
+        .activitiesBoxList {
+            width: 100%;
+            height: 390px;
+            border: 1px solid #d7d7d7;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 9px;
+        }
     }
-    .activitiesBoxList {
-        width: 100px;
-        height: 100px;
-        background-color: yellow;
-    }
+`;
+
+const Activity = styled.div`
+    width: 256px;
+    height: 79px;
+    background-color: #f1f1f1;
+    border-radius: 5px;
 `;
